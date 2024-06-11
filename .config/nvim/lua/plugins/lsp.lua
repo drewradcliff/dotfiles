bdc89c6a1fc47d17326d4d45ed675b9c9b7d4d0d
Drew <sirampr@gmail.com>
2024-06-10 23:54:53 -0400

    lsp updates

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local on_attach = require("lsp-format").on_attach
-      lspconfig.gleam.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-    end,
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     local lspconfig = require("lspconfig")
+  --     local on_attach = require("lsp-format").on_attach
+  --     lspconfig.gleam.setup({
+  --       capabilities = capabilities,
+  --       on_attach = on_attach,
+  --     })
+  --   end,
+  -- },
   {
     "lukas-reineke/format.nvim",
     config = function()
       local on_attach = require("lsp-format").on_attach
       local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require("lsp-format").setup({})
 
-      lspconfig.gleam.setup({ on_attach = on_attach })
+      lspconfig.gleam.setup({ on_attach = on_attach, capabilities = capabilities })
     end,
   },
 }
